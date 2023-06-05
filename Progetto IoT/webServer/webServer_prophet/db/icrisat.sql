dfd1250e3e81988eb65634314ee65ac5e56a8099
Lorenzofini <lorenzo.fini2@studio.unibo.it>
2023-06-05 18:04:10 +0200

    the end

--- Progetto IoT/webServer/webServer_prophet/db/icrisat.sql
+++ Progetto IoT/webServer/webServer_prophet/db/icrisat.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Creato il: Mag 14, 2023 alle 22:05
--- Versione del server: 10.4.22-MariaDB
--- Versione PHP: 8.1.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `icrisat_1`
---
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `sheet1`
---
 
 CREATE TABLE `sheet1` (
   `Station` varchar(10) DEFAULT NULL,
@@ -37,10 +12,6 @@ CREATE TABLE `sheet1` (
   `Wind` double DEFAULT NULL,
   `Rain` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dump dei dati per la tabella `sheet1`
---
 
 INSERT INTO `sheet1` (`Station`, `Date`, `MaxT`, `MinT`, `RH1`, `RH2`, `Wind`, `Rain`) VALUES
 ('ICRISAT', '01/01/1978', 28.5, 14.2, 68, 31, 5.7, 0),
@@ -6656,7 +6627,3 @@ INSERT INTO `sheet1` (`Station`, `Date`, `MaxT`, `MinT`, `RH1`, `RH2`, `Wind`, `
 ('ESP8266', '12/30/2022', 6.3, 2.4, 87, 34, 2.1, 0.4),
 ('ESP8266', '01/02/2023', 8.3, 5.4, 77, 44, 0.1, 0);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
